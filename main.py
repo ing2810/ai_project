aa1b65298fb94aa92de95308b2338011a42b219c
ing2810 <leearam2071@gmail.com>
2026-05-13 13:46:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ a=st.text_input('이름을 입력하세요')
 b=st.selectbox('좋아하는 음식을 선택하라.',['휘낭시에','크레이프','에그타르트','케이크','스콘'])
 if st.button('인삿말 생성'):
   st.write(a+'님, 안녕하냐? 반갑습니다.')
+  st.info('반갑다.')
+  st.warning(b+'음식을 좋아하시는군.')
+  st.error('잘 부탁하다.')
